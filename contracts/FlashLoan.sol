20f47f3b3dc8d8e303c069d202cbcb17a6375022
hdsfade <552037917@qq.com>
2023-04-23 14:05:28 +0800

    Move the functions of path to libraries

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -13,10 +13,13 @@ import {ILido} from "./interfaces/LIDO/ILido.sol";
 import {IComet} from "./interfaces/COMP/IComet.sol";
 import {IPoolDataProvider} from "./interfaces/AAVE/IPoolDataProvider.sol";
 import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import './libraries/Path.sol';
 
 import "hardhat/console.sol";
 
 contract FlashLoan is IFlashLoanSimpleReceiver {
+    using Path for bytes;
+
     struct BaseSwapParams {
         bytes path;
         bool single;
@@ -49,15 +52,6 @@ contract FlashLoan is IFlashLoanSimpleReceiver {
     address payable public WSTADDRESS =
         payable(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0);
     address public USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-
-    /// @dev The length of the bytes encoded address
-    uint256 private constant ADDR_SIZE = 20;
-    /// @dev The length of the bytes encoded fee
-    uint256 private constant FEE_SIZE = 3;
-    /// @dev The offset of a single token address and pool fee
-    uint256 private constant NEXT_OFFSET = ADDR_SIZE + FEE_SIZE;
-    /// @dev The offset of an encoded pool key
-    uint256 private constant POP_OFFSET = NEXT_OFFSET + ADDR_SIZE;
 
     constructor(address provider, address swapRouter, address owner) {
         ADDRESSES_PROVIDER = IPoolAddressesProvider(provider);
@@ -180,7 +174,7 @@ contract FlashLoan is IFlashLoanSimpleReceiver {
     function AaveOperation(
         BaseSwapParams calldata baseSwapParams
     ) public returns (bool) {
-        (, address Long, ) = decodeLastPool(baseSwapParams.path);
+        (, address Long, ) = baseSwapParams.path.decodeLastPool();
 
         SwapParams memory swapParams = SwapParams({
             base: baseSwapParams,
@@ -198,8 +192,8 @@ contract FlashLoan is IFlashLoanSimpleReceiver {
         uint256 interestRateMode,
         ApprovePermitParams calldata permitParams
     ) public returns (bool) {
-        (address Long, , ) = decodeFirstPool(base.path);
-        (, address Short, ) = decodeLastPool(base.path);
+        (address Long, , ) = base.path.decodeFirstPool();
+        (, address Short, ) = base.path.decodeLastPool();
         IERC20(Short).approve(address(POOL), flashAmount);
         uint256 repayAmount = POOL.repay(
             Short,
@@ -255,8 +249,8 @@ contract FlashLoan is IFlashLoanSimpleReceiver {
         BaseSwapParams calldata base,
         uint256 flashAmount
     ) public returns (bool) {
-        (address Long, , ) = decodeFirstPool(base.path);
-        (, address Short, ) = decodeLastPool(base.path);
+        (address Long, , ) = base.path.decodeFirstPool();
+        (, address Short, ) = base.path.decodeLastPool();
         IERC20(Short).approve(address(COMET), flashAmount);
 
         uint256 balance = IERC20(Short).balanceOf(address(this));
@@ -297,7 +291,7 @@ contract FlashLoan is IFlashLoanSimpleReceiver {
         uint256 flashAmount
     ) public returns (bool) {
         address initiator = tx.origin;
-        (, address Long, ) = decodeLastPool(base.path);
+        (, address Long, ) = base.path.decodeLastPool();
 
         IERC20(Long).approve(address(COMET), flashAmount);
         COMET.supplyTo(initiator, Long, flashAmount);
@@ -379,7 +373,7 @@ contract FlashLoan is IFlashLoanSimpleReceiver {
         uint256 amountIn,
         uint256 amountOutMinimum
     ) internal returns (uint256 amountOut) {
-        (address tokenIn, address tokenOut, uint24 fee) = decodeFirstPool(path);
+        (address tokenIn, address tokenOut, uint24 fee) = path.decodeFirstPool();
 
         console.log("tokenIn:", tokenIn);
         console.log("tokenOut:", tokenOut);
@@ -407,7 +401,7 @@ contract FlashLoan is IFlashLoanSimpleReceiver {
         uint256 amountIn,
         uint256 amountOutMinimum
     ) internal returns (uint256 amountOut) {
-        (address tokenIn, , ) = decodeFirstPool(path);
+        (address tokenIn, , ) = path.decodeFirstPool();
 
         _safeApprove(tokenIn, address(SWAP_ROUTER), amountIn);
         // IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
@@ -424,98 +418,7 @@ contract FlashLoan is IFlashLoanSimpleReceiver {
         amountOut = SWAP_ROUTER.exactInput(params);
     }
 
-    /// @notice Decodes the first pool in path
-    /// @param path The bytes encoded swap path
-    /// @return tokenA The first token of the given pool
-    /// @return tokenB The second token of the given pool
-    /// @return fee The fee level of the pool
-    function decodeFirstPool(
-        bytes memory path
-    ) internal pure returns (address tokenA, address tokenB, uint24 fee) {
-        tokenA = toAddress(path, 0);
-        fee = toUint24(path, ADDR_SIZE);
-        tokenB = toAddress(path, NEXT_OFFSET);
-    }
 
-    function decodeLastPool(
-        bytes memory path
-    ) internal pure returns (address tokenA, address tokenB, uint24 fee) {
-        uint256 len = path.length;
-        tokenA = toAddress(path, len - POP_OFFSET);
-        fee = toUint24(path, len - NEXT_OFFSET);
-        tokenB = toAddress(path, len - ADDR_SIZE);
-    }
-
-    /// @dev toAddress decodes bytes to address
-    function toAddress(
-        bytes memory _bytes,
-        uint256 _start
-    ) internal pure returns (address) {
-        require(_start + 20 >= _start, "toAddress_overflow");
-        require(_bytes.length >= _start + 20, "toAddress_outOfBounds");
-        address tempAddress;
-
-        assembly {
-            tempAddress := div(
-                mload(add(add(_bytes, 0x20), _start)),
-                0x1000000000000000000000000
-            )
-        }
-
-        return tempAddress;
-    }
-
-    /// @dev toUint24 decodes bytes to uint24
-    function toUint24(
-        bytes memory _bytes,
-        uint256 _start
-    ) internal pure returns (uint24) {
-        require(_start + 3 >= _start, "toUint24_overflow");
-        require(_bytes.length >= _start + 3, "toUint24_outOfBounds");
-        uint24 tempUint;
-
-        assembly {
-            tempUint := mload(add(add(_bytes, 0x3), _start))
-        }
-
-        return tempUint;
-    }
-
-    /// @dev toBool decodes bytes to bool
-    function toBool(
-        bytes memory _bytes,
-        uint256 _start
-    ) internal pure returns (bool tempBool) {
-        require(_start + 1 >= _start, "toBool_overflow");
-        require(_bytes.length >= _start + 1, "toBool_outOfBounds");
-        uint8 temp;
-
-        assembly {
-            temp := mload(add(add(_bytes, 0x1), _start))
-        }
-
-        if (temp == 1) {
-            tempBool = true;
-        }
-
-        return tempBool;
-    }
-
-    /// @dev toUint256 decodes bytes to uint256
-    function toUint256(
-        bytes memory _bytes,
-        uint256 _start
-    ) internal pure returns (uint256) {
-        require(_start + 32 >= _start, "toUint256_overflow");
-        require(_bytes.length >= _start + 32, "toUint256_outOfBounds");
-        uint256 tempUint;
-
-        assembly {
-            tempUint := mload(add(add(_bytes, 0x20), _start))
-        }
-
-        return tempUint;
-    }
 
     function _safeApprove(address token, address to, uint256 value) internal {
         (bool success, bytes memory data) = token.call(
